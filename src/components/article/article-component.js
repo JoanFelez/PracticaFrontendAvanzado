c206c63a8c2878f563a843bba6c381cbe9ff4772
Joan Felez <joan.felez@movicoders.com>
2018-10-10 17:57:18 +0200

    Starting author info

--- src/components/article/article-component.js
+++ src/components/article/article-component.js
@@ -1,13 +1,14 @@
 
 export const newArticle = ({id, title, image, description, author, published, comments} =
 {id: '', title: 'No title', published: 'No date', comments: '0',
-    author: {name: 'No author', photo: 'No photo', desc: 'No author desciption'}}) =>{
+    author: {name: 'No author', photo: 'https://image.shutterstock.com/image-vector/blank-avatar-photo-place-holder-450w-1095249842.jpg', desc: 'No author desciption'}}) =>{
     const article = document.createElement('div');
     article.classList.add('article-container');
     article.innerHTML = `
         <div class="article-info">
             <a class="article-link" href="/article/?id=${id}"><h3>${title}</h3></a>
             <p class="article-description">${description}</p>
+            <a class="author-name" href="#">${author.name}</a>
             <div class="author-info">
                 <a class="author-name" href="#">${author.name}</a>
                 <div class="author-image"><img src="${author.photo}" alt="Autor Photo">Author Photo</div>
